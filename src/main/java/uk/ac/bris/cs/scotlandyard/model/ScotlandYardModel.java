8dcbc72e4865d09f81a46053ff05c66856e60e3b
dANW34V3R <dan.weaver@hotmail.co.uk>
2019-04-02 15:05:18 +0100

    first 36 tests pass

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -2,8 +2,10 @@ package uk.ac.bris.cs.scotlandyard.model;
 
 import java.util.*;
 
+import ch.qos.logback.core.pattern.color.BlackCompositeConverter;
 import org.checkerframework.checker.nullness.qual.RequiresNonNull;
 import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
 
 import static java.util.Objects.requireNonNull;
 import static uk.ac.bris.cs.scotlandyard.model.Colour.BLACK;
@@ -14,7 +16,10 @@ public class ScotlandYardModel implements ScotlandYardGame {
 	List<Boolean> rounds;
 	Graph<Integer, Transport> graph;
 	List<ScotlandYardPlayer> players;
-
+	int playerIndex;
+	int currentRound = 0;
+	boolean isGameOver = false;
+	int mrXLastLocation = 0;
 
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
@@ -22,6 +27,9 @@ public class ScotlandYardModel implements ScotlandYardGame {
 
 		this.rounds = requireNonNull(rounds);
 		this.graph = requireNonNull(graph);
+		players = new ArrayList<>();
+
+
 
 		if (rounds.isEmpty()) {
 			throw new IllegalArgumentException("Empty rounds");
@@ -43,6 +51,7 @@ public class ScotlandYardModel implements ScotlandYardGame {
 
 		Set<Integer> set = new HashSet<>();
 		Set<Colour> setColour = new HashSet<>();
+		Integer n = 0;
 
 		for (PlayerConfiguration configuration : configurations) {
 			if (set.contains(configuration.location))
@@ -54,26 +63,20 @@ public class ScotlandYardModel implements ScotlandYardGame {
 			setColour.add(configuration.colour);
 
 			if (!configuration.equals(mrX)) {
-
 				if (!(configuration.tickets.containsKey(Ticket.valueOf("TAXI")) && configuration.tickets.containsKey(Ticket.valueOf("BUS")) && configuration.tickets.containsKey(Ticket.valueOf("UNDERGROUND")) && configuration.tickets.containsKey(Ticket.valueOf("SECRET")) && configuration.tickets.containsKey(Ticket.valueOf("DOUBLE"))))
 					throw new IllegalArgumentException("Detectives don't contain correct tickets");
 				if (!(configuration.tickets.get(Ticket.valueOf("SECRET")) == 0 && configuration.tickets.get(Ticket.valueOf("DOUBLE")) == 0))
 					throw new IllegalArgumentException("Detectives contain SECRET or DOUBLE ticket");
-
 			}else{
+				playerIndex = n;
 				if (!(configuration.tickets.containsKey(Ticket.valueOf("TAXI")) && configuration.tickets.containsKey(Ticket.valueOf("BUS")) && configuration.tickets.containsKey(Ticket.valueOf("UNDERGROUND")) && configuration.tickets.containsKey(Ticket.valueOf("DOUBLE")) && configuration.tickets.containsKey(Ticket.valueOf("SECRET"))))
 					throw new IllegalArgumentException("MrX doesn't contain correct tickets");
 			}
 
+
 			players.add(new ScotlandYardPlayer(configuration.player, configuration.colour, configuration.location, configuration.tickets));
+			n++;
 		}
-
-
-
-
-
-
-
 	}
 
 	@Override
@@ -102,56 +105,64 @@ public class ScotlandYardModel implements ScotlandYardGame {
 
 	@Override
 	public List<Colour> getPlayers() {
-		// TODO
-		throw new RuntimeException("Implement me");
+		List<Colour> playerColours = new ArrayList<>();
+		for(ScotlandYardPlayer player : players){
+			playerColours.add(player.colour());
+		}
+		return Collections.unmodifiableList(playerColours);
 	}
 
 	@Override
 	public Set<Colour> getWinningPlayers() {
-		// TODO
-		throw new RuntimeException("Implement me");
+		return Collections.emptySet();
 	}
 
 	@Override
 	public Optional<Integer> getPlayerLocation(Colour colour) {
-		// TODO
-		throw new RuntimeException("Implement me");
+		if(colour == BLACK){
+			return Optional.of(mrXLastLocation);
+		}
+		for(ScotlandYardPlayer player : players){
+			if(player.colour() == colour){
+				return Optional.of(player.location());
+			}
+		}
+		return Optional.empty();
 	}
 
 	@Override
 	public Optional<Integer> getPlayerTickets(Colour colour, Ticket ticket) {
-		// TODO
-		throw new RuntimeException("Implement me");
+		for(ScotlandYardPlayer player : players){
+			if(player.colour() == colour){
+				return Optional.of(player.tickets().get(ticket));
+			}
+		}
+		return Optional.empty();
 	}
 
 	@Override
 	public boolean isGameOver() {
-		// TODO
-		throw new RuntimeException("Implement me");
+		return isGameOver;
 	}
 
 	@Override
 	public Colour getCurrentPlayer() {
-		// TODO
-		throw new RuntimeException("Implement me");
+		return players.get(playerIndex).colour();
 	}
 
 	@Override
 	public int getCurrentRound() {
-		// TODO
-		throw new RuntimeException("Implement me");
+		return currentRound;
 	}
 
 	@Override
 	public List<Boolean> getRounds() {
-		// TODO
-		throw new RuntimeException("Implement me");
+		return Collections.unmodifiableList(rounds);
 	}
 
 	@Override
 	public Graph<Integer, Transport> getGraph() {
-		// TODO
-		throw new RuntimeException("Implement me");
+		return new ImmutableGraph<>(graph);
 	}
 
 }
